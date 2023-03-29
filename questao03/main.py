21ebe1b246f65ae2500e79899b4f7c83d8cd498f
João Pedro Guimarães <joaopedroguimaraes96@gmail.com>
2023-03-29 02:32:51 -0300

    Versão incompleta da questão 03 por erro no site

---
+++ questao03/main.py
@@ -0,0 +1,13 @@
+import os
+
+site = 'http://coopertotal.nc7i.com/'
+user = os.getenv('LOGIN_USUARIO')
+senha = os.getenv('LOGIN_SENHA')
+
+farmacia = '01.621.293/0002-78 - TESTE PORTAL - NÃO FATURAR'
+condicoes = 'CONDIÇÃO DIAMANTE A PRAZO'
+pgto_prazo = '42 dias'
+pgto_dados = 'boleto bancário'
+
+# Questão incompleta, o site não estava funcionando
+# Anexei uma imagem ao repositório para demonstrar o erro
